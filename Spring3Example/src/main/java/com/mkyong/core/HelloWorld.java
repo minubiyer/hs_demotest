5b0e05cd2136d4fc3689f03aa78637a28bf51509
Meena B Iyer <meena.b.iyer@accenture.com>
2016-05-12 11:11:49 +0530

    Story2, added new property age

--- Spring3Example/src/main/java/com/mkyong/core/HelloWorld.java
+++ Spring3Example/src/main/java/com/mkyong/core/HelloWorld.java
@@ -8,6 +8,17 @@ public class HelloWorld {
 	private String name;
 	//developing new functionality in dev branch
 	private String careerPath;
+	//Developing Sprint Story 2, will span for a week
+	private String age;
+	
+	public void setAge(String age) {
+		this.age=age;
+	}
+	
+	public String getAge() {
+		return age;
+	}
+	
 	
 	public void setCareerPath(String cPath) {
 		careerPath=cPath;
