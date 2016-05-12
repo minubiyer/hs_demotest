00d6ce87a0968cd3719156dfc36e1330895d7998
Meena B Iyer <meena.b.iyer@accenture.com>
2016-05-12 13:40:40 +0530

    cherry picked story 5 changes

--- Spring3Example/src/main/java/com/mkyong/core/HelloWorld.java
+++ Spring3Example/src/main/java/com/mkyong/core/HelloWorld.java
@@ -8,6 +8,15 @@ public class HelloWorld {
 	private String name;
 	//developing new functionality in dev branch
 	private String careerPath;
+	//Developing Sprint5... nw lets see how merge takes place
+	
+	public void setAge(String age) {
+		this.age=age;
+	}
+	
+	public String getAge() {
+		return age;
+	}
 	
 	public void setCareerPath(String cPath) {
 		careerPath=cPath;
