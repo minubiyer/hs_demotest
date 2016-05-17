3586ccbe116ebda9e86693ea4388df3c66a379e2
Meena B Iyer <meena.b.iyer@accenture.com>
2016-05-17 11:45:52 +0530

    JIRA102, completed adding fucntionality

--- Spring3Example/src/main/java/com/mkyong/core/CatalogueManager.java
+++ Spring3Example/src/main/java/com/mkyong/core/CatalogueManager.java
@@ -4,4 +4,8 @@ public class CatalogueManager {
 	public static void main(String[] args) {
 		System.out.println("Added feature#1 user story change");
 	}
+	
+	public String invokeSomething() {
+		System.out.println("hahahahah");
+	}
 }
