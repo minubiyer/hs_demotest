ffb9352509f3662bf93fd1e4d7025fad93d0bac0
Meena B Iyer <meena.b.iyer@accenture.com>
2016-05-17 11:47:24 +0530

    DEFECT101, defect fixed

--- Spring3Example/src/main/java/com/mkyong/core/CatalogueManager.java
+++ Spring3Example/src/main/java/com/mkyong/core/CatalogueManager.java
@@ -3,5 +3,6 @@ package com.mkyong.core;
 public class CatalogueManager {
 	public static void main(String[] args) {
 		System.out.println("Added feature#1 user story change");
+		System.out.println("Fixing defect");
 	}
 }
