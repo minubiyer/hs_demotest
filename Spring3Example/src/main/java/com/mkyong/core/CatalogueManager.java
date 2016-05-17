e9561e0d8b071b06f1e9b8b14820f7ca9f60c35f
Meena B Iyer <meena.b.iyer@accenture.com>
2016-05-17 11:44:52 +0530

    JIRA101, completed adding new functionality to CM

--- Spring3Example/src/main/java/com/mkyong/core/CatalogueManager.java
+++ Spring3Example/src/main/java/com/mkyong/core/CatalogueManager.java
@@ -4,4 +4,9 @@ public class CatalogueManager {
 	public static void main(String[] args) {
 		System.out.println("Added feature#1 user story change");
 	}
+
+
+	public static void main1(String[] args) {
+		System.out.println("Added feature#1 user story change");
+	}
 }
