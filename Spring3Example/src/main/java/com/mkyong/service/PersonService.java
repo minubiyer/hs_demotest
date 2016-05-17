2f03295098618acd48accf9d4255e2a746302c9b
Meena B Iyer <meena.b.iyer@accenture.com>
2016-05-17 18:25:08 +0530

    Story#1, JIRA 112 - Added PersonService Implementation

---
+++ Spring3Example/src/main/java/com/mkyong/service/PersonService.java
@@ -0,0 +1,10 @@
+package com.mkyong.service;
+
+
+public class PersonService {
+	
+	@Webservice
+	public Account getAccountDetails(String accountNo) {
+		//Connect to Database and extract account details
+	}
+}
